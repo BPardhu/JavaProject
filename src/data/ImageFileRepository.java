722ac777bacec132a150b44eb4a6473ccdad048a
BPardhu <commanderjamessolo@gmail.com>
2026-05-05 21:29:53 +0530

    Add files via upload

---
+++ src/data/ImageFileRepository.java
@@ -0,0 +1,18 @@
+package data;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ImageFileRepository {
+
+    public static BufferedImage readImage(File file) throws Exception {
+        return ImageIO.read(file);
+    }
+
+    public static boolean writeImage(
+            BufferedImage image, String format, File outputFile
+    ) throws Exception {
+        return ImageIO.write(image, format, outputFile);
+    }
+}
